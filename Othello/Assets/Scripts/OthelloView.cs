d59367651f3054ddb97fe7d656015a973f3e72d1
Leon Akasaka <leon@wisdomsoft.jp>
2015-10-29 12:52:03 +0900

    オセロゲームの追加。

---
+++ Othello/Assets/Scripts/OthelloView.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class OthelloView : MonoBehaviour
+{
+	void Start ()
+    {
+        var cell = Resources.Load<GameObject>("Cell/Cell");
+        Instantiate(cell);
+	}
+}
